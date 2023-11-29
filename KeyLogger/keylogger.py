4087251713f1c319e4283d916831d4ad95adddee
hg <gaskinshannah@gmail.com>
2023-11-29 06:39:25 -0800

    Keylogger
    Added introductory keylogger code and removed Testing123.md file

---
+++ KeyLogger/keylogger.py
@@ -0,0 +1,16 @@
+import pynput
+
+# From pynput.keyboard import Key, Listener
+
+def on_key_press(key):
+	print("(0) pressed".format(key))
+
+
+def on_key_release(key):
+	if key == Key.esc:
+		return False
+
+
+with Lisenter(on_key_press=on_key_press, on_key_release=on_key_release) as listener:
+	listener.join()
+
